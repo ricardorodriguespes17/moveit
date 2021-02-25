5d6aa6ebba2bf8ebd21f1fbca21d5c17c7841115
Ricardo Rodrigues <39037180+ricardorodriguespes17@users.noreply.github.com>
2021-02-25 20:52:11 -0300

    create completed challenges

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from '../styles/components/CompletedChallenges.module.css'
+
+function CompletedChallenges() {
+  return (
+    <div className={styles.completedChallengesContainer}>
+      <span>Desafios completos</span>
+      <span>5</span>
+    </div>
+  )
+}
+
+export default CompletedChallenges;
