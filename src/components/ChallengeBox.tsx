4dd60fb636845d777cbcaf09026a702614d49b4b
Ricardo Rodrigues <39037180+ricardorodriguespes17@users.noreply.github.com>
2021-02-26 22:00:50 -0300

    challenge box component

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import styles from '../styles/components/ChallengeBox.module.css'
+
+function ChallengeBox() {
+
+  const hasActiveChallenge = true;
+
+  return (
+    <div className={styles.challengeBoxContainer}>
+      {hasActiveChallenge ? (
+        <div className={styles.challengeActive}>
+          <header>Ganhe 400 xp</header>
+
+          <main>
+            <img src="icons/body.svg" alt="" />
+            <strong>Novo desafio</strong>
+            <p>Levente e faça uma caminhada de 3 minutos.</p>
+          </main>
+
+          <footer>
+            <button type="button" className={styles.challengeFailButton}>Falhei</button>
+            <button type="button" className={styles.challengeSucceededButton}>Completei</button>
+          </footer>
+        </div>
+      ) : (
+        <div className={styles.challengeNotActive}>
+        <strong>Finalize um ciclo para receber um desafio</strong>
+        <p>
+          <img src="icons/level-up.svg" alt="Level up" />
+          Avance de level completando desafios.
+        </p>
+      </div>
+      )}
+    </div>
+  )
+}
+
+export default ChallengeBox;
